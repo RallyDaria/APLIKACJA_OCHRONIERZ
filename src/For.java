c7c6bf1a96bdbe4e2f29229806b44cdc273faca8
RallyDaria <dariazblewska@gmail.com>
2022-10-25 20:09:08 +0200

    Tablica kręcenia Kotkóws

---
+++ src/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) {
+String [] catTable=new String[]{"Kocislaw","Kokos","Julian","Kotlet"};
+for (int i=0;i<catTable.length;i++){
+    System.out.println(catTable[i]);
+        }
+    }
+}
