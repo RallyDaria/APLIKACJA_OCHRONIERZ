d0c0f964e45303db17bff017043b2704bd09aecf
RallyDaria <dariazblewska@gmail.com>
2022-10-25 20:39:52 +0200

    douwhile

--- src/DouWhile.java
+++ src/DouWhile.java
@@ -1,5 +1,10 @@
 public class DouWhile {
     public static void main(String[] args) {
+        int count=0;
+        do{
+            count++;
+            System.out.println(count);
 
+                   }while (count<10);
     }
 }
