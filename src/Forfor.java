aec5f74085cc32a12206b8d1d77676ac0980de55
RallyDaria <dariazblewska@gmail.com>
2022-10-25 21:25:37 +0200

    forfor daria

---
+++ src/Forfor.java
@@ -0,0 +1,10 @@
+public class Forfor {
+    public static void main(String[] args) {
+
+        for ( int i=0; i < 10; i=i+2){
+            System.out.println("Cześć Daria");
+        }
+
+
+    }
+}
