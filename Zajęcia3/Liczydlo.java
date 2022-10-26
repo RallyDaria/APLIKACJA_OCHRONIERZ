3b32f029250bbfab44459152192be63cc53b1a8e
RallyDaria <dariazblewska@gmail.com>
2022-10-26 18:37:39 +0200

    liczydloTomaszdziała

--- Zajęcia3/Liczydlo.java
+++ Zajęcia3/Liczydlo.java
@@ -1,6 +1,6 @@
 public class Liczydlo {
     public static void main(String[] args) {
-    int [] liczydlo=new int [] {2,3,7,3,3,5,2};
+    int [] liczydlo=new int [] {2,3,7,9,8,6,3,3,3,3,5,2};
     int count=0;
     for (int cyfra: liczydlo){
         if (cyfra==3){
