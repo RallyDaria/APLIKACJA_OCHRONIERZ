79451128b802612b6caf5d1551c749007dde07af
RallyDaria <dariazblewska@gmail.com>
2022-10-26 18:50:35 +0200

    CAR obiekt w który uderzamy

--- src/ZajeciaObiekty/Car.java
+++ src/ZajeciaObiekty/Car.java
@@ -7,7 +7,10 @@ public class Car {
     String colour;
 
     Car(int wheels, int seats, int lights, String colour){
-
+        this.wheels=wheels;
+        this.seats=seats;
+        this.lights=lights;
+        this.colour=colour;
     }
 
 }
