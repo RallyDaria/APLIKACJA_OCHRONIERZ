8863b5b6644965d6cd8b6916d7202d8c3b5aa0e7
RallyDaria <dariazblewska@gmail.com>
2022-10-26 18:33:10 +0200

    Liczydlo2

--- Zajęcia3/Liczydlo2.java
+++ Zajęcia3/Liczydlo2.java
@@ -1,2 +1,13 @@
 public class Liczydlo2 {
+    public static void main(String[] args) {
+        int[] licz3 = new int[]{3, 4, 5, 6, 3, 3, 3, 5, 5, 6, 7, 3};
+        int count=0;
+        for (int i = 0; i < 12; i++) {
+            if (licz3[i] == 3) ;{
+             count++;
+
+            }
+        } System.out.println(count);
+
+    }
 }
